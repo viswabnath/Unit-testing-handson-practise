2bc9e70aeae5259fbde96d75f073317d703d1761
Bhanu <gautam.bhanu8@gmail.com>
2021-02-25 09:18:46 +0530

    new test cases

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -1,5 +1,5 @@
 import { html, fixture, expect } from '@open-wc/testing';
-
+import { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
 
 describe('Basic details', () => {
@@ -10,23 +10,48 @@ describe('Basic details', () => {
     expect(inputElement.type).to.equal('text');
     // expect(el.shadowRoot.querySelector('lion-input').ad).to.equal('Loan Details');
   });
-});
 
-/* describe('Test captureDetails', () => {
-  it('calls myFunction when a button is clicked', () => {
-    const el = fixture(
-      html`
-        <basic-details
-          ><lion-form><form></form></lion-form
-        ></basic-details>
-      `
+  it('calls capturedetails function when a button is clicked', async () => {
+    const el = await fixture(
+      html`<basic-details
+        ><lion-form><form></form></lion-form
+      ></basic-details>`
     );
-    const myFunctionStub = stub(el, '_toCustomer');
-    // const formEl = el.dom.querySelector('form');
-
+    const myFunctionStub = stub(el, '_captureDetails');
+    el.requestUpdate();
+    await el.updateComplete;
     el.shadowRoot.querySelector('button').click();
-    // el.submit(myFunctionStub);
+    expect(myFunctionStub).to.have.callCount(0);
+  });
+
+  it('calls dashboard function when a button is clicked', async () => {
+    const el = await fixture(
+      html`<basic-details
+        ><lion-form><form></form></lion-form
+      ></basic-details>`
+    );
+    const myFunctionStub = stub(el, '_toDashboard');
+    el.requestUpdate();
+    await el.updateComplete;
+    el.shadowRoot.querySelector('button').click();
     expect(myFunctionStub).to.have.callCount(1);
   });
 });
- */
+
+//  describe('Test captureDetails', () => {
+//   it('calls myFunction when a button is clicked', () => {
+//     const el = fixture(
+//       html`
+//         <basic-details
+//           ><lion-form><form></form></lion-form
+//         ></basic-details>
+//       `
+//     );
+//     const myFunctionStub = stub(el, '_toCustomer');
+//     // const formEl = el.dom.querySelector('form');
+
+//     el.shadowRoot.querySelector('button').click();
+//     // el.submit(myFunctionStub);
+//     expect(myFunctionStub).to.have.callCount(1);
+//   });
+// });
--- test/customer-details.test.js
+++ test/customer-details.test.js
@@ -1,5 +1,5 @@
 import { html, fixture, expect } from '@open-wc/testing';
-
+import { stub } from 'sinon';
 import '../src/Customer/Customer-details.js';
 
 describe('customer details', () => {
@@ -8,5 +8,18 @@ describe('customer details', () => {
     const inputElement = el.shadowRoot.querySelector('lion-input');
 
     expect(inputElement.type).to.equal('text');
+  });
+
+  it('calls emidetails function when a button is clicked', async () => {
+    const el = await fixture(
+      html`<customer-details
+        ><lion-form><form></form></lion-form
+      ></customer-details>`
+    );
+    const myFunctionStub = stub(el, '_toEmidetails');
+    el.requestUpdate();
+    await el.updateComplete;
+    el.shadowRoot.querySelector('button').click();
+    expect(myFunctionStub).to.have.callCount(0);
   });
 });
