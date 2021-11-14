85aa905a35cc9e83e1d9496311d998eb42910e1b
Arpan <armukherjee89@gmail.com>
2021-11-14 13:59:32 +0530

    test case scenario

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -3,55 +3,5 @@ import { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
 
 describe('Basic details', () => {
-  it('Trail and error test case', async () => {
-    const el = await fixture(html`<basic-details></basic-details>`);
-    const inputElement = el.shadowRoot.querySelector('lion-input');
-
-    expect(inputElement.type).to.equal('text');
-    // expect(el.shadowRoot.querySelector('lion-input').ad).to.equal('Loan Details');
-  });
-
-  it('calls capturedetails function when a button is clicked', async () => {
-    const el = await fixture(
-      html`<basic-details
-        ><lion-form><form></form></lion-form
-      ></basic-details>`
-    );
-    const myFunctionStub = stub(el, '_captureDetails');
-    el.requestUpdate();
-    await el.updateComplete;
-    el.shadowRoot.querySelector('button').click();
-    expect(myFunctionStub).to.have.callCount(0);
-  });
-
-  it('calls dashboard function when a button is clicked', async () => {
-    const el = await fixture(
-      html`<basic-details
-        ><lion-form><form></form></lion-form
-      ></basic-details>`
-    );
-    const myFunctionStub = stub(el, '_toDashboard');
-    el.requestUpdate();
-    await el.updateComplete;
-    el.shadowRoot.querySelector('button').click();
-    expect(myFunctionStub).to.have.callCount(1);
-  });
+  // Write test cases inside this block
 });
-
-//  describe('Test captureDetails', () => {
-//   it('calls myFunction when a button is clicked', () => {
-//     const el = fixture(
-//       html`
-//         <basic-details
-//           ><lion-form><form></form></lion-form
-//         ></basic-details>
-//       `
-//     );
-//     const myFunctionStub = stub(el, '_toCustomer');
-//     // const formEl = el.dom.querySelector('form');
-
-//     el.shadowRoot.querySelector('button').click();
-//     // el.submit(myFunctionStub);
-//     expect(myFunctionStub).to.have.callCount(1);
-//   });
-// });
